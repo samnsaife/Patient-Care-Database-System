7290bef1392106513f4cca8117969ae1d0e7ae89
Sami Noor Saifi <samnsaifiF17@gmail.com>
2026-03-23 20:03:53 +0530

    Updating Js

---
+++ js/AdminLTE.js
@@ -0,0 +1,7 @@
+import CardRefresh from './CardRefresh'
+import CardWidget from './CardWidget'
+import ControlSidebar from './ControlSidebar'
+import DirectChat from './DirectChat'
+import Dropdown from './Dropdown'
+import ExpandableTable from './ExpandableTable'
+
---
+++ js/IFrame.js
@@ -0,0 +1,20 @@
+/**
+ * --------------------------------------------
+ * AdminLTE IFrame.js
+ * License MIT
+ * --------------------------------------------
+ */
+
+import $ from 'jquery'
+
+/**
+ * Constants
+ * ====================================================
+ */
+
+const NAME = 'IFrame'
+const DATA_KEY = 'lte.iframe'
+const JQUERY_NO_CONFLICT = $.fn[NAME]
+
+const SELECTOR_DATA_TOGGLE = '[data-widget="iframe"]'
+const SELECTOR_DATA_TOGGLE_CLOSE = '[data-widget="iframe-close"]'
