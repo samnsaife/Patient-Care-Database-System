208554824574ad99d1d1bdc72dcdb658078f03c8
Sami Noor Saifi <samnsaifiF17@gmail.com>
2026-03-23 20:00:26 +0530

    Recharging Database

---
+++ database/hprms_db.sql
@@ -0,0 +1,13 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2021 at 08:41 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
