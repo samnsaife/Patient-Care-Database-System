a16f945c62c65f4d478b37a68809349eeb71d0ad
Sami Noor Saifi <samnsaifiF17@gmail.com>
2026-03-24 21:11:07 +0530

    Update view_room.php

--- admin/rooms/view_room.php
+++ admin/rooms/view_room.php
@@ -8,4 +8,28 @@ if(isset($_GET['id'])){
             if(!is_numeric($k))
             $$k = $v;
         }
-   
+    }
+}
+?>
+<style>
+    #uni_modal .modal-footer{
+        display:none !important;
+    }
+</style>
+<div class="container-fluid">
+    <dl>
+        <dt class="text-muted">Room Name</dt>
+        <dd class='pl-4 fs-4 fw-bold'><?= isset($name) ? $name : '' ?></dd>
+        <dt class="text-muted">Room Type</dt>
+        <dd class='pl-4 fs-4 fw-bold'><?= isset($room) ? $room : '' ?></dd>
+        <dt class="text-muted">Description</dt>
+        <dd class='pl-4'>
+            <p class=""><small><?= isset($description) ? html_entity_decode($description) : '' ?></small></p>
+        </dd>
+        <dt class="text-muted">Capacity</dt>
+        <dd class='pl-4 fs-4 fw-bold'><?= isset($capacity) ? ($capacity) : 0 ?></dd>
+    </dl>
+    <div class="col-12 text-right">
+        <button class="btn btn-flat btn-sm btn-dark" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+    </div>
+</div>
