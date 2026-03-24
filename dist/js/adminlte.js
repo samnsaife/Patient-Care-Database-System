24a2fc602552718309bfdde87beea7f3f78e3b66
Sami Noor Saifi <samnsaifiF17@gmail.com>
2026-03-24 13:35:33 +0530

    Update adminlte.js

--- dist/js/adminlte.js
+++ dist/js/adminlte.js
@@ -10,16 +10,6 @@
 
     var $__default = /*#__PURE__*/ _interopDefaultLegacy($);
 
-    /**
-     * --------------------------------------------
-     * AdminLTE CardRefresh.js
-     * License MIT
-     * --------------------------------------------
-     */
-    /**
-     * Constants
-     * ====================================================
-     */
 
     var NAME = 'CardRefresh';
     var DATA_KEY = 'lte.cardrefresh';
